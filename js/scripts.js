a53dfd329393ce5b343fb39dc6bb591cf783293a
Robert Murray and Victor Puentes <robert@>
2017-07-11 10:35:18 -0700

    add functionality to display the array and also made so that the final count does not go above the max number.

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,15 @@ var theCount = [];
 $(function() {
   $("#daForm").submit(function(event) {
     event.preventDefault();
+    // the value of the input is the argument for parseInt
     maxNumber = parseInt($("input#countTo").val());
     countBy = parseInt($("input#countBy").val());
     var currentNumber = 0;
-
-    for (var index = 0; currentNumber < maxNumber; index += 1) {
+    //to avoid current number going above max number, pre-count once to make sure it doesn't go above the max number
+    for (var index = 0; (currentNumber+countBy) <= maxNumber; index += 1) {
       currentNumber += countBy;
       theCount[index] = currentNumber;
     }
+    $("#output").text(theCount);
   });
 });
